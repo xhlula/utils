83e28aa324efd8a3de3743f0769ae0662d2e8ab4
Aldo <aldo.mateli@gmail.com>
2015-04-20 22:00:41 +0200

    initial commit

---
+++ src/maldoinc/utils/Pagination.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace maldoinc\utils;
+
+class Pagination
+{
+    /**
+     * @var int number of records per page
+     */
+    public $recordsPerPage;
+
+    /**
+     * @var int current page number
+     */
+    protected $currentPage;
+
+    /**
+     * @var int number of total records
+     */
+    public $totalRecords;
+
+    /**
+     * @var int number of visible pages
+     */
+    public $nrPagesVisible;
+
+    /**
+     * @var bool show/hide first/last page links
+     */
+    public $showFirstLast = true;
+
+    /**
+     * @var string label for first page link
+     */
+    public $firstPageStr = 'First';
+
+    /**
+     * @var string label for last page link
+     */
+    public $lastPageStr = 'Last';
+
+    /**
+     * @param int $total the total number of records
+     * @param int $per_page number of records visible per page
+     * @param int $page current page
+     * @param int $nr_pages_visible number of visible pages
+     */
+    public function __construct($total, $per_page, $page, $nr_pages_visible)
+    {
+        $this->recordsPerPage = $per_page;
+        $this->totalRecords = $total;
+        $this->nrPagesVisible = $nr_pages_visible;
+        $this->setCurrentPage($page);
+    }
+
+    public function getCurrentPage()
+    {
+        return $this->currentPage;
+    }
+
+    /**
+     * Sets the current page. Value is casted to int before being assigned.
+     * If the value is out of bounds it is defaulted to 1
+     *
+     * @param int $value
+     */
+    public function setCurrentPage($value)
+    {
+        $value = (int)$value;
+
+        if (($value >= 1) && ($value <= $this->getTotalPages())) {
+            $this->currentPage = $value;
+        } else {
+            // silently default to 1 if current page value is not valid
+            $this->currentPage = 1;
+        }
+    }
+
+    /**
+     * Returns the number of records to be skipped
+     *
+     * @return int
+     */
+    public function getLimitOffset()
+    {
+        return ($this->currentPage - 1) * $this->recordsPerPage;
+    }
+
+    /**
+     * Returns the number of total pages available
+     *
+     * @return float
+     */
+    public function getTotalPages()
+    {
+        return floor($this->totalRecords / $this->recordsPerPage) + (int)($this->totalRecords % $this->recordsPerPage > 0);
+    }
+
+    /**
+     * Returns the pages to be rendered specified by the current settings
+     *
+     * @return array
+     */
+    public function getPages()
+    {
+        // in case of no data return an empty pagination array
+        if ($this->totalRecords == 0) {
+            return array();
+        }
+
+        $f = floor($this->nrPagesVisible / 2);
+        $start = $this->currentPage - $f;
+        $end = $this->currentPage + $f;
+
+        if ($end < $this->nrPagesVisible) {
+            $end = $this->nrPagesVisible;
+        }
+
+        if ($end > $this->getTotalPages()) {
+            $end = $this->getTotalPages();
+        }
+
+        if ($end - $start < $this->nrPagesVisible) {
+            $start = 1 + $end - $this->nrPagesVisible;
+        }
+
+        return range(max(1, $start), $end);
+    }
+
+    /**
+     * Generates HTML based on the pages to be rendered and the callback function
+     *
+     * @param callable $reducefunc callback to the function which will return the markup
+     * @return string
+     */
+    public function getHTML($reducefunc)
+    {
+        $html = "";
+        $pages = $this->getPages();
+        $cnt = count($pages);
+        $total = $this->getTotalPages();
+
+        if ($this->showFirstLast && (($cnt > 0) && ($pages[0] != 1))) {
+            $html = $reducefunc(1, $this->firstPageStr);
+        }
+
+        $html .= array_reduce($pages, function ($result, $item) use ($reducefunc) {
+            return $result . $reducefunc($item, $item);
+        });
+
+        if ($this->showFirstLast && (($cnt > 0) && ($pages[$cnt - 1] != $total))) {
+            $html .= $reducefunc($total, $this->lastPageStr);
+        }
+
+        return $html;
+    }
+}
---
+++ tests/utils/PaginationTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use maldoinc\utils\Pagination;
+
+class PaginationTest extends PHPUnit_Framework_TestCase
+{
+    public function testPageCount()
+    {
+        $p = 9;
+        $pagination = new Pagination(100, 5, 1, $p);
+
+        $this->assertEquals(count($pagination->getPages()), $p);
+        $this->assertEquals(count((new Pagination(0, 5, 1, 9))->getPages()), 0);
+
+    }
+
+    public function testPageValues()
+    {
+        // test the pages to be rendered. 9 items visible at the time.
+        // pagination should start at 5 and end at 13
+        $pagination = new Pagination(100, 5, 9, 9);
+
+        $this->assertEquals($pagination->getPages(), array(5, 6, 7, 8, 9, 10, 11, 12, 13));
+    }
+
+    public function testSmallData()
+    {
+        $pagination = new Pagination(1, 5, 1, 9);
+
+        $this->assertEquals($pagination->getPages(), array(1));
+    }
+
+    public function testGetLimitOffset()
+    {
+        $pagination = new Pagination(100, $per_page = 5, 1, 9);
+
+        $this->assertEquals($pagination->getLimitOffset(), 0);
+
+        $pagination->setCurrentPage(5);
+        $this->assertEquals($pagination->getLimitOffset(), 4 * $per_page);
+    }
+
+    public function testGetHTML()
+    {
+        $cb = function ($page, $label) {
+            return $page;
+        };
+
+        $pagination = new Pagination(100, 10, 1, 9);
+        $pagination->showFirstLast = false;
+
+        $this->assertEquals('123456789', $pagination->getHTML($cb));
+
+
+        $pagination = new Pagination(200, 10, 1, 9);
+        $pagination->setCurrentPage(10);
+        $pagination->showFirstLast = true;
+        $this->assertEquals('16789101112131420', $pagination->getHTML($cb));
+    }
+
+    public function testSetPage()
+    {
+        $pagination = new Pagination(1, 5, 1, 9);
+
+        $pagination->setCurrentPage(-1);
+        $this->assertEquals($pagination->getCurrentPage(), 1);
+
+        $pagination->setCurrentPage("hello world");
+        $this->assertEquals($pagination->getCurrentPage(), 1);
+    }
+
+    public function testEndPages()
+    {
+        $pagination = new Pagination(100, 5, 19, 9);
+
+        $this->assertEquals($pagination->getPages(), array(12, 13, 14, 15, 16, 17, 18, 19, 20));
+    }
+}
